0d1628cb3f647ecc218e3b48cd5ef31c31a2bd85
Erik Yang <eyang7@berkeley.edu>
2022-04-29 00:16:51 -0700

    working

--- src/Category.js
+++ src/Category.js
@@ -4,38 +4,36 @@ import {useState} from "react";
 
 
 // constants that change state 
-const [value, setValue] = useState(); //value of the input form 
 
 // functional component to return 
-const Category = () => {
-    return (
-        <div className = "category">
-            <button className = "category-button" onClick={() => AddTab()}> New Tab </button>
-            
-            {/* want to return tab value as well */}
+// const Category = () => {    
+//     return (
+//         <div className = "category">
+//             <button className = "category-button" onClick={() => AddTab()}> New Tab </button>
+//         </div>
 
-        </div>
+//     ); 
+// }
 
-    ); 
-}
+// export default Category; 
 
-export default Category; 
+$('form').submit(function(e) {
+    e.preventDefault(); 
+
+    $('#tabs').append($('<li id="list">').text($('#m').val()));
+})
 
 // need to create a tab component using dictionary of tab name to metadata 
 
-// Helper functions 
+// helper functions
 function AddTab() {
-
-    const onFormSubmit = (event) => {
-          event.preventDefault(); 
-          const username = event.target[0].element; 
-          setValue(username.value); 
-    }; 
-  
     return (
-        <form onSubmit={onFormSubmit}>
-        <input type="text" id = "username"/>
-        <input type = "submit"/> 
+        <form> 
+            <input id="m" placeholder="Enter your message..." autocomplete="off" required />
         </form>
     ); 
 }
+
+// <div className = "category">
+//     <button className = "category-button" onClick={() => AddTab()}> New Tab </button>
+//  </div>
--- src/Organizer.jsx
+++ src/Organizer.jsx
@@ -1,5 +1,6 @@
 import React from "react"; 
 import Navbar from './Navbar'; 
+import Category from './Category'; 
 import {useState} from "react"; 
 
 // const [tabs, addToTab] = useState([]);  //empty list of tab components 
@@ -8,8 +9,16 @@ const Organizer = () => {
     return(
         <div className="app">
             <Navbar/> 
+            
+            {/* this is where teh tabs will go */}
+            <ul id = "tabs"></ul>
+            <form> 
+                <input id="m" placeholder="Enter your message..." autocomplete="off" required />
+            </form>
+
+        <script src="Category.js"></script>
         </div>
-        
+
         // {/* how to return a value(name) to add to addToTab ==> need a GET request to get info */}
         // {/* <div> <Category/> </div>  */}
         // {/*create a tab component if necessary, and then add to tabs state*/}
