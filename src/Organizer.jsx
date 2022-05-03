d97146c0672955d641c207fd8e6113a4b30d1f75
Erik Yang <eyang7@berkeley.edu>
2022-05-02 18:29:23 -0700

    working on tab component

--- src/Organizer.jsx
+++ src/Organizer.jsx
@@ -1,6 +1,7 @@
 import React from "react"; 
 import Navbar from './Navbar'; 
 import Category from './Category'; 
+import Button from "react-bootstrap/Button"; 
 import {useState} from "react"; 
 
 // const [tabs, addToTab] = useState([]);  //empty list of tab components 
@@ -15,6 +16,7 @@ const Organizer = () => {
     // helper functions
     const handleSubmit = (e) => {
         e.preventDefault(); 
+        // const newTab = CreateTab(curr); 
         updateTabs([...tabs, curr]); 
         updateIndex(0); 
         updateCurr(""); 
@@ -37,23 +39,67 @@ const Organizer = () => {
             <form onSubmit = {(e) => handleSubmit(e)}> 
                 <input type = "text" value = {curr} placeholder="Add new tab..." autocomplete="off" required 
                 onChange = {(e) => {updateCurr(curr + e.target.value[index]); updateIndex(index +1)}}/>
+                {/* add if value is backspace delete the index */}
             </form>
             </div>
 
+
             {/* this is where teh tabs will go */}
-            <ul id = "tabs">
+
+            <div className = "tabs">
+            <ul id = "tabs-list">
             <div className = "newTab"> {DisplayTabs()} </div>
             </ul>
+            </div>
 
 
             {/* clear button that resets everything */}
+            <Button id = "reset"/*onClick={}*/> Reset </Button>
         </div>
 
-        // {/* how to return a value(name) to add to addToTab ==> need a GET request to get info */}
-        // {/*create a tab component if necessary, and then add to tabs state*/
         // {/* each tab data using Node.js and then display tabs through frontend */}
-        // {/* DisplayTabs();  */}
     ); 
 }
 
 export default Organizer; 
+
+// tab component 
+function CreateTab(name) {
+
+    function DisplayTabItem() {
+        if (isSubmit) {
+            return (
+                items.map((item) => <li>{item} </li>)  //fix mapping all arrays
+            ); 
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        updateItems([...items, curr]); 
+        updateSubmit(true); 
+    }
+
+    const [tabIndex, updateIndex] = useState(0); 
+    const [curr, updateCurr] = useState(""); 
+    const [isSubmit, updateSubmit] = useState(false); 
+    const [items, updateItems] = useState([]); 
+
+    return (
+        <div className = "tab"> 
+            <h3> {name} </h3>
+
+            <ul className = "tab-list"> {DisplayTabItem()} </ul>
+
+            <div className = "tab-items"> 
+            <form onSubmit = {(e) => handleSubmit(e)}> 
+                <input type = "text" value = {curr} placeholder="List new item..." autocomplete="off" required 
+                onChange = {(e) => {updateCurr(curr + e.target.value[tabIndex]); updateIndex(tabIndex +1)}}/>
+            </form>
+            </div>
+        </div>
+    ); 
+}
+
+
+// might need a backend so that the data can persist from server to server 
