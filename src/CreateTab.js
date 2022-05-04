6c3ed642190ea13ec0cdb9d296758691d5956816
ishanthewizard <77704916+ishanthewizard@users.noreply.github.com>
2022-05-03 18:42:31 -0700

    Fixed tabs

---
+++ src/CreateTab.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+import {ListItem} from './ListItem'
+export const CreateTab = ({name}) => {
+
+  const handleSubmit = (e) => {
+      e.preventDefault(); 
+      setItems([...items, curr ]);  
+      updateCurr(""); 
+  }
+
+  const [items, setItems] = useState([])
+  const [curr, updateCurr] = useState(""); 
+
+  function removeItem(key) {
+      const new_items = items.filter((_item, idx) => idx !== key)
+      setItems(new_items)
+  }
+  return (
+      <div className = "tab"> 
+          <h3> {name} </h3>
+          <ul className = "tab-list"> {items.map((item, idx) => <ListItem key= {idx} key2 = {idx} text= {item} remove= {removeItem}  />)} </ul>
+
+          <div className = "tab-items"> 
+          <form onSubmit = {(e) => handleSubmit(e)}> 
+              <input type = "text" value = {curr} placeholder="List new item..."
+              onChange = {(e) => {updateCurr(e.target.value)}}/>
+          </form>
+          </div>
+      </div>
+  ); 
+}
--- src/Organizer.jsx
+++ src/Organizer.jsx
@@ -1,66 +1,40 @@
-import React from "react"; 
+import React, {useState} from "react"; 
 import Navbar from './Navbar'; 
 import Category from './Category'; 
 import Button from "react-bootstrap/Button"; 
-import {useState} from "react"; 
-
-// const [tabs, addToTab] = useState([]);  //empty list of tab components 
-
+import { CreateTab } from "./CreateTab";  //empty list of tab components 
+import { Flex } from "@chakra-ui/react";
 const Organizer = () => {
-    // const states
-    const [isSubmit, changeSubmit] = useState(false); 
-    const [tabs, updateTabs] = useState([]);
-    const [curr, updateCurr] = useState(""); 
-    const [index, updateIndex] = useState(0); 
-
-    // helper functions
     const handleSubmit = (e) => {
         e.preventDefault(); 
-        // const newTab = CreateTab(curr); 
-        updateTabs([...tabs, curr]); 
-        updateIndex(0); 
+        setItems([...items, curr ]); 
+        console.log(curr)
         updateCurr(""); 
-        changeSubmit(true);  //TODO: fix bc everytime it will now be true and display everything
     }
-
-    function DisplayTabs() {
-        if (isSubmit) {
-            return (
-                tabs.map((tab) => <li>{tab} </li>)  //fix mapping all arrays
-            ); 
-        }
-    }
-    
+    const [items, setItems] = useState([])
+    const [curr, updateCurr] = useState(""); 
     return (
-        <div> 
-            {CreateTab("test")}
-        </div>
-    ); 
-    // return(
-    //     <div className="app">
-    //         <Navbar/> 
-            
-    //         <div className = "category"> 
-    //         <form onSubmit = {(e) => handleSubmit(e)}> 
-    //             <input type = "text" value = {curr} placeholder="Add new tab..." autocomplete="off" required 
-    //             onChange = {(e) => {updateCurr(curr + e.target.value[index]); updateIndex(index +1)}}/>
-    //             {/* add if value is backspace delete the index */}
-    //         </form>
-    //         </div>
+        <div className="app">
+            <Navbar/> 
 
+            <div className = "category"> 
+            <form onSubmit = {(e) => handleSubmit(e)}> 
+                <input type = "text" value = {curr} placeholder="Add new tab..."
+                onChange = {(e) => updateCurr(e.target.value)}/>
+            </form>
+            </div>
 
-    //         {/* this is where teh tabs will go */}
+             {/* this is where the tabs will go */}
 
-    //         <div className = "tabs">
-    //         <ul id = "tabs-list">
-    //         <div className = "newTab"> {DisplayTabs()} </div>
-    //         </ul>
-    //         </div>
-
-
-    //         {/* clear button that resets everything */}
-    //         <Button id = "reset"/*onClick={}*/> Reset </Button>
-    //     </div>
+             <div className = "tabs">
+             <ul id = "tabs-list">
+             <Flex flexWrap= "wrap" spacing= "300px" > {items.map((item, idx) => <CreateTab key= {idx} name= {item} />)} </Flex>
+             </ul>
+             </div>
+             {/* clear button that resets everything */}
+             <Button id = "reset"/*onClick={}*/> Reset </Button>
+         </div>
+    )
 
     //     // {/* each tab data using Node.js and then display tabs through frontend */}
     // ); 
@@ -69,61 +43,7 @@ const Organizer = () => {
 export default Organizer; 
 
 // tab component => 
-const CreateTab = (name) => {
 
-    function DisplayTabItem() {
-        if (isSubmit) {
-            return (
-                items.map((item) => <li id = "tab-item">
-                    <CheckBox/> 
-                    {item} </li>)  //fix mapping all arrays
-            ); 
-        }
-    }
-
-    const CheckBox = () => {
-        const [checked, setChecked] = useState(false); 
-    
-        const handleChange = () => {
-            setChecked(!checked); 
-        }
-        
-        return (
-            <label>
-                <input type = "checkbox"
-                    checked = {checked} onChange = {handleChange}/>
-            </label>
-        ); 
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault(); 
-        updateItems([...items, curr]); 
-        updateSubmit(true); 
-        updateIndex(0); 
-        updateCurr(""); 
-    }
-
-    const [tabIndex, updateIndex] = useState(0); 
-    const [curr, updateCurr] = useState(""); 
-    const [isSubmit, updateSubmit] = useState(false); 
-    const [items, updateItems] = useState([]); 
-
-    return (
-        <div className = "tab"> 
-            <h3> {name} </h3>
-
-            <ul className = "tab-list"> {DisplayTabItem()} </ul>
-
-            <div className = "tab-items"> 
-            <form onSubmit = {(e) => handleSubmit(e)}> 
-                <input type = "text" value = {curr} placeholder="List new item..." autocomplete="off" required 
-                onChange = {(e) => {updateCurr(curr + e.target.value[tabIndex]); updateIndex(tabIndex +1)}}/>
-            </form>
-            </div>
-        </div>
-    ); 
-}
 
 
 
